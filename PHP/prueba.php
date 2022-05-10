a1d565bb1b74fce7a142a6fa127980db68423ccb
conanCR7Siuuu <93269858+conanCR7Siuuu@users.noreply.github.com>
2022-05-10 13:16:05 +0200

    Add files via upload

---
+++ PHP/prueba.php
@@ -0,0 +1,14 @@
+<?php
+echo <<<HTML
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+ <meta charset=\"UTF-8\">
+ <title>Hola mundo</title>
+</head>
+<body>
+ <h1>Hola mundo</h1>
+</body>
+</html>
+HTML;
+?>
